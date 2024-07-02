2d59e983f0beb099e33226a142ce203be081288d
Jemuel <jem123maglupay@gmail.com>
2024-07-02 10:27:31 +0800

    first commit

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,11 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
+    fave_set_1 = set(fave_numbers_1)
+
     for number in fave_numbers_2:
-        if number not in fave_numbers_1:
+        # if number not in fave_numbers_1:
+        if number not in fave_set_1:
             return False
 
     return True
--- phasebook/search.py
+++ phasebook/search.py
@@ -24,7 +24,51 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
+    id = args.get('id')
+    name = args.get('name')
+    age = args.get('age')
+    occupation = args.get('occupation')
+    print(id, name, age, occupation)
+    search_result = set()
+
+    if id:
+        for user in USERS:
+            if user['id'] == id:
+                search_result.add(tuple(user.items()))
+                break
+    if name:
+        for user in USERS:
+            if name.lower() in user['name'].lower():
+                search_result.add(tuple(user.items()))
+    if age:
+        for user in USERS:
+            user_age = int(user['age'])
+            age = int(age)
+            if age - 1 <= user_age <= age + 1:
+                search_result.add(tuple(user.items()))
+
+    if occupation:
+        for user in USERS:
+            if occupation.lower() in user['occupation'].lower():
+                search_result.add(tuple(user.items()))
+
+    new_search_result = [dict(user) for user in search_result]
+
+    def sort_by_priority(user):
+        id_match = 1 if id and user['id'] == id else 0
+        name_match = 1 if name and name.lower() in user['name'].lower() else 0
+        age_match = 1 if age and age - 1 <= int(user['age']) <= age + 1 else 0
+        occupation_match = 1 if occupation and occupation.lower(
+        ) in user['occupation'].lower() else 0
+
+        priority_score = (id_match * 4) + (name_match * 3) + \
+            (age_match * 2) + (occupation_match * 1)
+        print(priority_score)
+
+        return priority_score
+
+    new_search_result.sort(key=sort_by_priority, reverse=True)
 
     # Implement search here!
-
-    return USERS
+    return new_search_result
+    # return search_result
